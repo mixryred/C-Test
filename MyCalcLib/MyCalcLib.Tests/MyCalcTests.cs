66f750697ecaa6f78ef23ea7bdf8c0b37fd5aa0a
Artem <pavlyk.artem@gmail.com>
2020-09-29 00:19:33 +0300

    Koj

---
+++ MyCalcLib/MyCalcLib.Tests/MyCalcTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcLib.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Sum_30and40_70returned()
+        {
+            int x = 30;
+            int y = 40;
+            int expected = 70;
+
+            MyCalc c = new MyCalc();
+            int actual = c.Sum(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyCalcLib/MyCalcLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCalcLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCalcLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0024438c-2ec9-471b-869a-ff4b3ad8853c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
